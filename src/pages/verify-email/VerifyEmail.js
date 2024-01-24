e4fa52e4a1891e376b9ec3f9bc732057deb63c50
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2024-01-24 22:03:22 +0200

    frontend-blog

--- src/pages/verify-email/VerifyEmail.js
+++ src/pages/verify-email/VerifyEmail.js
@@ -54,7 +54,14 @@ const VerifyEmail = () => {
         </div>
       ) : (
         <>
-          <NotFound/>
+                    <div className='box-border   xl:w-[704px] lg:w-[704] md:w-full sm:w-full xl:absolute lg:absolute  md:static sm:static h-auto rounded-lg xl:bg-[#f4f7f957] lg:bg-[#f4f7f957] sm:bg-[#d3e6f257] backdrop-blur-[15px] 2xl:mb-0 xl:mb-0 lg:mb-0 md:mb-5 sm:mb-5 p-10 text-center flex flex-col justify-start items-center space-y-4 '>
+          <span className="text-3xl text-blue-black">
+            Invalid Link
+           </span>
+          <Link to="/login" className="text-2xl font-[600] text-very-blue">
+            Go To Login Page
+          </Link>
+        </div>
         </>
       )}</>
 
